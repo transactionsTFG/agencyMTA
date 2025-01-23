05c4059b09438f16e0613d69d8ee9cd51d140a91
josevillacres46 <jvillacr@ucm.es>
2025-01-23 20:34:25 +0100

    FEAT: Add DB in Glassfish

---
+++ src/main/java/integration/environment/JUnitEnv.java
@@ -0,0 +1,14 @@
+package integration.environment;
+
+public class JUnitEnv extends PersistenceConfig {
+
+	@Override
+	public boolean isRunningJUnit() {
+		return true;
+	}
+
+	@Override
+	public String getNamePersistance() {
+		return "MtaAgencyTest";
+	}
+}
