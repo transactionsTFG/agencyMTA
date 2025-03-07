918cd53e157df7ba056a8bf297851d00e3c30131
josevillacres46 <jvillacr@ucm.es>
2025-03-07 17:15:27 +0100

    FIX: NoArgsConstructor

--- src/main/java/common/dto/soap/request/IdFlightInstanceWithSeatsSOAP.java
+++ src/main/java/common/dto/soap/request/IdFlightInstanceWithSeatsSOAP.java
@@ -2,9 +2,11 @@ package common.dto.soap.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class IdFlightInstanceWithSeatsSOAP {
     private long idFlightInstance;
     private int seats;
