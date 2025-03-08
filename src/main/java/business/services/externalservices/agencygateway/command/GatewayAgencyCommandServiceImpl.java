7b6374250eccdea1e0803870a348ff61a12f18fa
danielfdez17 <daniel17dev@gmail.com>
2025-03-08 09:40:53 +0100

    FIX: same yesterday's check with idFlightInstanceWithSeatsMap added to modFlightAndHotel

--- src/main/java/business/services/externalservices/agencygateway/command/GatewayAgencyCommandServiceImpl.java
+++ src/main/java/business/services/externalservices/agencygateway/command/GatewayAgencyCommandServiceImpl.java
@@ -26,16 +26,19 @@ import common.mapper.TravelMapper;
 
 @Stateless
 public class GatewayAgencyCommandServiceImpl implements GatewayAgencyCommandService {
-    
+
     private UserService userService;
     private AgencyTravelService agencyTravelService;
     private AirlineReservationCommandService airlineReservationCommandService;
     private HotelBookingCommandService hotelBookingCommandService;
 
-    public GatewayAgencyCommandServiceImpl() {}
+    public GatewayAgencyCommandServiceImpl() {
+    }
 
     @Inject
-    public GatewayAgencyCommandServiceImpl(UserService userService, AgencyTravelService agencyTravelService, AirlineReservationCommandService airlineReservationCommandService, HotelBookingCommandService hotelBookingCommandService) {
+    public GatewayAgencyCommandServiceImpl(UserService userService, AgencyTravelService agencyTravelService,
+            AirlineReservationCommandService airlineReservationCommandService,
+            HotelBookingCommandService hotelBookingCommandService) {
         this.userService = userService;
         this.agencyTravelService = agencyTravelService;
         this.airlineReservationCommandService = airlineReservationCommandService;
@@ -43,25 +46,31 @@ public class GatewayAgencyCommandServiceImpl implements GatewayAgencyCommandServ
     }
 
     @Override
-    public HotelFlightReservationDTO getHotelAndFlightReservationWithLockMode(long hotelBookingId, long flightReservationId) {
-        ReservationDTO reservationDTO = this.airlineReservationCommandService.getAirlineReservationWithLockMode(flightReservationId);
+    public HotelFlightReservationDTO getHotelAndFlightReservationWithLockMode(long hotelBookingId,
+            long flightReservationId) {
+        ReservationDTO reservationDTO = this.airlineReservationCommandService
+                .getAirlineReservationWithLockMode(flightReservationId);
         BookingDTO bookingDTO = this.hotelBookingCommandService.getBookingWithLockMode(hotelBookingId);
         return new HotelFlightReservationDTO(bookingDTO, reservationDTO);
     }
 
     @Override
-    public AgencyReservationSuccessDTO makeReservationFlight(final long idCustomer, final String dni, Map<Long, Integer> mapIdFlightInstance) {
+    public AgencyReservationSuccessDTO makeReservationFlight(final long idCustomer, final String dni,
+            Map<Long, Integer> mapIdFlightInstance) {
         UserDTO userDTO = this.userService.readUserById(idCustomer);
-        List<IdFlightWithSeatsDTO> idFlightWithSeatsDTOs = mapIdFlightInstance.entrySet().stream().map(entry -> new IdFlightWithSeatsDTO(entry.getKey(), entry.getValue())).toList();
-        AirlineReservationDTO airlineMTAReservationDTO = this.airlineReservationCommandService.makeReservation(userDTO, dni, idFlightWithSeatsDTOs);
+        List<IdFlightWithSeatsDTO> idFlightWithSeatsDTOs = mapIdFlightInstance.entrySet().stream()
+                .map(entry -> new IdFlightWithSeatsDTO(entry.getKey(), entry.getValue())).toList();
+        AirlineReservationDTO airlineMTAReservationDTO = this.airlineReservationCommandService.makeReservation(userDTO,
+                dni, idFlightWithSeatsDTOs);
         airlineMTAReservationDTO.getStatusFlightsDTO()
-                                .forEach(status -> this.agencyTravelService.makeTravel(TravelMapper.INSTANCE.toMakeReservationAirline(airlineMTAReservationDTO, status, idCustomer)));
+                .forEach(status -> this.agencyTravelService.makeTravel(
+                        TravelMapper.INSTANCE.toMakeReservationAirline(airlineMTAReservationDTO, status, idCustomer)));
         return AgencyReservationSuccessDTO.builder()
-                    .dateReservation(airlineMTAReservationDTO.getDate())
-                    .idReservation(airlineMTAReservationDTO.getId())
-                    .statusFlightsDTO(airlineMTAReservationDTO.getStatusFlightsDTO())
-                    .totalReservation(airlineMTAReservationDTO.getTotal())
-                    .build();
+                .dateReservation(airlineMTAReservationDTO.getDate())
+                .idReservation(airlineMTAReservationDTO.getId())
+                .statusFlightsDTO(airlineMTAReservationDTO.getStatusFlightsDTO())
+                .totalReservation(airlineMTAReservationDTO.getTotal())
+                .build();
     }
 
     @Override
@@ -73,14 +82,18 @@ public class GatewayAgencyCommandServiceImpl implements GatewayAgencyCommandServ
         return bookingHotel;
     }
 
-    
     @Override
-    public AgencyReservationSuccessDTO makeReservationHotelAndFlight(final MakeBookingReservationDTO booking, long idCustomer, String dni, Map<Long, Integer> mapIdFlightInstance) {
+    public AgencyReservationSuccessDTO makeReservationHotelAndFlight(final MakeBookingReservationDTO booking,
+            long idCustomer, String dni, Map<Long, Integer> mapIdFlightInstance) {
         UserDTO userDTO = this.userService.readUserById(idCustomer);
-        List<IdFlightWithSeatsDTO> idFlightWithSeatsDTOs = mapIdFlightInstance.entrySet().stream().map(entry -> new IdFlightWithSeatsDTO(entry.getKey(), entry.getValue())).toList();
+        List<IdFlightWithSeatsDTO> idFlightWithSeatsDTOs = mapIdFlightInstance.entrySet().stream()
+                .map(entry -> new IdFlightWithSeatsDTO(entry.getKey(), entry.getValue())).toList();
         BookingDTO bookingHotel = this.hotelBookingCommandService.makeBooking(booking, userDTO, dni);
-        AirlineReservationDTO airlineMTAReservationDTO = this.airlineReservationCommandService.makeReservation(userDTO, dni, idFlightWithSeatsDTOs);
-        airlineMTAReservationDTO.getStatusFlightsDTO().forEach(status -> this.agencyTravelService.makeTravel(TravelMapper.INSTANCE.toMakeReservationAirlineAndHotel(bookingHotel, airlineMTAReservationDTO, status, idCustomer)));
+        AirlineReservationDTO airlineMTAReservationDTO = this.airlineReservationCommandService.makeReservation(userDTO,
+                dni, idFlightWithSeatsDTOs);
+        airlineMTAReservationDTO.getStatusFlightsDTO()
+                .forEach(status -> this.agencyTravelService.makeTravel(TravelMapper.INSTANCE
+                        .toMakeReservationAirlineAndHotel(bookingHotel, airlineMTAReservationDTO, status, idCustomer)));
         return AgencyReservationSuccessDTO.builder()
                 .dateReservation(airlineMTAReservationDTO.getDate())
                 .idBooking(bookingHotel.getId())
@@ -89,22 +102,25 @@ public class GatewayAgencyCommandServiceImpl implements GatewayAgencyCommandServ
                 .totalReservation(airlineMTAReservationDTO.getTotal() + bookingHotel.getTotalPrice())
                 .build();
     }
- 
+
     @Override
-    public UpdateReservationDTO modifyReservationFlight(final long idReservation, final Map<Long, Integer> idFlightInstanceWithSeatsMap) {
-        UpdateReservationDTO updateReservationDTO = this.airlineReservationCommandService.modifyReservation(idReservation, idFlightInstanceWithSeatsMap);
+    public UpdateReservationDTO modifyReservationFlight(final long idReservation,
+            final Map<Long, Integer> idFlightInstanceWithSeatsMap) {
+        UpdateReservationDTO updateReservationDTO = this.airlineReservationCommandService
+                .modifyReservation(idReservation, idFlightInstanceWithSeatsMap);
         List<TravelDTO> listTravel = this.agencyTravelService.findTravelByIdReservationFlight(idReservation);
-        if(listTravel.isEmpty()) 
+        if (listTravel.isEmpty())
             throw new TravelException("No se encontraron viajes asociados a la reserva de vuelo");
         listTravel.forEach(travel -> {
             final long idTravelFlight = travel.getFlightReservationID();
-            if (!idFlightInstanceWithSeatsMap.containsKey(idTravelFlight)) 
+            if (!idFlightInstanceWithSeatsMap.containsKey(idTravelFlight))
                 return;
             final int numberSeats = idFlightInstanceWithSeatsMap.get(idTravelFlight);
-            final double price = updateReservationDTO.getUpdatePriceReservationDTO().stream().filter(f -> f.getIdFlightInstance() == idTravelFlight).findFirst().get().getPrice();
-            if(numberSeats == 0)
+            final double price = updateReservationDTO.getUpdatePriceReservationDTO().stream()
+                    .filter(f -> f.getIdFlightInstance() == idTravelFlight).findFirst().get().getPrice();
+            if (numberSeats == 0)
                 travel.setActive(false);
-            
+
             travel.setPassengerCounter(numberSeats);
             travel.setCost(updateReservationDTO.getTotal());
             travel.setFlightCost(price);
@@ -117,7 +133,7 @@ public class GatewayAgencyCommandServiceImpl implements GatewayAgencyCommandServ
     public BookingDTO modifyReservationHotel(ModifyBookingReservationDTO booking) {
         BookingDTO bookingHotel = this.hotelBookingCommandService.modifyBooking(booking);
         TravelDTO travelDTO = this.agencyTravelService.findTravelByIdReservationHotel(booking.getId());
-        travelDTO.setActive(true); 
+        travelDTO.setActive(true);
         travelDTO.setDate(booking.getStartDate());
         travelDTO.setReturnDate(booking.getEndDate());
         travelDTO.setPassengerCounter(bookingHotel.getPeopleNumber());
@@ -128,16 +144,22 @@ public class GatewayAgencyCommandServiceImpl implements GatewayAgencyCommandServ
     }
 
     @Override
-    public UpdateReservationDTO modifyHotelFlightReservation(final ModifyBookingReservationDTO booking, final long idReservation, final Map<Long, Integer> idFlightInstanceWithSeatsMap) {
-        UpdateReservationDTO updateReservationDTO = this.airlineReservationCommandService.modifyReservation(idReservation, idFlightInstanceWithSeatsMap);
+    public UpdateReservationDTO modifyHotelFlightReservation(final ModifyBookingReservationDTO booking,
+            final long idReservation, final Map<Long, Integer> idFlightInstanceWithSeatsMap) {
+        UpdateReservationDTO updateReservationDTO = this.airlineReservationCommandService
+                .modifyReservation(idReservation, idFlightInstanceWithSeatsMap);
         BookingDTO bookingHotel = this.hotelBookingCommandService.modifyBooking(booking);
-        List<TravelDTO> listTravel = this.agencyTravelService.findTravelByIdReservationFlightAndHotel(idReservation, booking.getId());
-        if(listTravel.isEmpty()) 
+        List<TravelDTO> listTravel = this.agencyTravelService.findTravelByIdReservationFlightAndHotel(idReservation,
+                booking.getId());
+        if (listTravel.isEmpty())
             throw new TravelException("No se encontraron viajes asociados a la reserva de vuelo");
         listTravel.forEach(travel -> {
             final long idTravelFlight = travel.getFlightReservationID();
+            if (!idFlightInstanceWithSeatsMap.containsKey(idTravelFlight))
+                return;
             final int numberSeats = idFlightInstanceWithSeatsMap.get(idTravelFlight);
-            final double price = updateReservationDTO.getUpdatePriceReservationDTO().stream().filter(f -> f.getIdFlightInstance() == idTravelFlight).findFirst().get().getPrice();
+            final double price = updateReservationDTO.getUpdatePriceReservationDTO().stream()
+                    .filter(f -> f.getIdFlightInstance() == idTravelFlight).findFirst().get().getPrice();
             travel.setPassengerCounter(numberSeats + bookingHotel.getPeopleNumber());
             travel.setCost(updateReservationDTO.getTotal() + bookingHotel.getTotalPrice());
             travel.setHotelCost(bookingHotel.getTotalPrice());
@@ -152,10 +174,11 @@ public class GatewayAgencyCommandServiceImpl implements GatewayAgencyCommandServ
     public double cancelReservationFlight(final long idReservationFlight) {
         double priceReturn = this.airlineReservationCommandService.cancelReservation(idReservationFlight);
         List<TravelDTO> listTravel = this.agencyTravelService.findTravelByIdReservationFlight(idReservationFlight);
-        if(listTravel.isEmpty()) 
+        if (listTravel.isEmpty())
             throw new TravelException("No se encontraron viajes asociados a la reserva de vuelo");
-        boolean isAllTravelFlightCancel = listTravel.stream().map(travel -> this.agencyTravelService.cancelTravel(travel.getId())).allMatch(isCancel -> isCancel);
-        if(!isAllTravelFlightCancel)   
+        boolean isAllTravelFlightCancel = listTravel.stream()
+                .map(travel -> this.agencyTravelService.cancelTravel(travel.getId())).allMatch(isCancel -> isCancel);
+        if (!isAllTravelFlightCancel)
             throw new TravelException("No se han podido cancelar todos los viajes asociados a la reserva de vuelo");
         return priceReturn;
     }
@@ -194,11 +217,13 @@ public class GatewayAgencyCommandServiceImpl implements GatewayAgencyCommandServ
     public double cancelHotelFlightReservation(long hotelBookingId, long flightReservationId) {
         double priceReturn = this.airlineReservationCommandService.cancelReservation(flightReservationId);
         double moneyReturned = this.hotelBookingCommandService.cancelBooking(hotelBookingId);
-        List<TravelDTO> listTravel = this.agencyTravelService.findTravelByIdReservationFlightAndHotel(flightReservationId, hotelBookingId);
-        if(listTravel.isEmpty()) 
+        List<TravelDTO> listTravel = this.agencyTravelService
+                .findTravelByIdReservationFlightAndHotel(flightReservationId, hotelBookingId);
+        if (listTravel.isEmpty())
             throw new TravelException("No se encontraron viajes asociados a la reserva de vuelo");
-        boolean isAllTravelFlightCancel = listTravel.stream().map(travel -> this.agencyTravelService.cancelTravel(travel.getId())).allMatch(isCancel -> isCancel);
-        if(!isAllTravelFlightCancel)   
+        boolean isAllTravelFlightCancel = listTravel.stream()
+                .map(travel -> this.agencyTravelService.cancelTravel(travel.getId())).allMatch(isCancel -> isCancel);
+        if (!isAllTravelFlightCancel)
             throw new TravelException("No se han podido cancelar todos los viajes asociados a la reserva de vuelo");
         return priceReturn + moneyReturned;
     }
